ba7eec9d5284148d907678e95f789276ff9054e0
HenriqueBFreitas <henriqueproacc@gmail.com>
2026-02-14 16:20:47 -0300

    feat: implement CRUD and configure CORS

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
 from .models import Pagina
 from .serializers import PaginaSerializer
 
@@ -7,4 +9,8 @@ from .serializers import PaginaSerializer
 
 class PaginaViewSet(ModelViewSet):
     queryset = Pagina.objects.all()
-    seryalizer_class = PaginaSerializer
+    serializer_class = PaginaSerializer
+    permission_classes = [IsAuthenticated]
+
+class LoginView(TokenObtainPairView):
+    pass
