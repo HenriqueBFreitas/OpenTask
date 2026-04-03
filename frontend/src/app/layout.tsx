65b2b592d568046e814f163838bad1f4713d5934
davidemoura-lgtm <davi.demourasud@gmail.com>
2026-04-03 20:25:52 -0300

    feat: initial login frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next';
+import { Inter, Lora } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+const lora = Lora({ subsets: ['latin'], variable: '--font-lora' });
+
+export const metadata: Metadata = {
+  title: 'StudyXP',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${inter.variable} ${lora.variable} ${inter.className}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
