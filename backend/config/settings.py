3dfd271a9025edb092cee432229552a195f7b0aa
HenriqueBFreitas <henriqueproacc@gmail.com>
2026-04-03 21:27:33 -0300

    refactor: move Django API into backend folder

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -140,3 +140,13 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS") == "True"
+
+if not CORS_ALLOW_ALL_ORIGINS:
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:3000",
+    ]
+
+CORS_ALLOW_HEADERS = [
+    'authorization',
+    'content-type',
+]
