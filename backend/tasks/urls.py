d8173c552dd4c21772653987f7b0df44f666a3a4
davi-sudekum <davi.demourasud@gmail.com>
2026-05-18 01:12:04 -0300

    fix: .env-local frontend

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -10,9 +10,9 @@ subtask_router = DefaultRouter()
 subtask_router.register(r'subtasks', SubTaskViewSet, basename='subtasks')
 
 urlpatterns = [
+    path('boards/', BoardView.as_view()),  # ← antes do router
     path('', include(router.urls)),
     path('', include(subtask_router.urls)),
-    path('boards/', BoardView.as_view()),
     path('<int:task_id>/files/', TaskFileListCreateView.as_view(), name='taskfile-list-create'),
     path('<int:task_id>/files/<int:pk>/', TaskFileDetailView.as_view(), name='taskfile-detail'),
 ]
